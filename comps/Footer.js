e3d83212c95a526491f41b152279a6d92ae85384
mgorem <oremgitonga22@gmail.com>
2022-05-03 02:28:05 +0300

    learning next.js

---
+++ comps/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <div>
+            Copyright 2022 Ninjalist
+        </div>
+     );
+}
+ 
+export default Footer;
---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return ( 
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+        </div>
+     );
+}
+ 
+export default About;
